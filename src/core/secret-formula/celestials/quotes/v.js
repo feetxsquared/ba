86eef601141b4ea913182f9f2423139e15ea1d2a
feetxsquared <feet.xsquared@gmail.com>
2024-07-12 20:35:54 +0200

    Update v.js
    e

--- src/core/secret-formula/celestials/quotes/v.js
+++ src/core/secret-formula/celestials/quotes/v.js
@@ -2,93 +2,81 @@ export const vQuotes = {
   initial: {
     id: 0,
     lines: [
-      "How pathetic..."
+      "hello my name is v"
     ],
   },
   unlock: {
     id: 1,
     lines: [
-      "Welcome to my Reality.",
-      "I am surprised you could reach it.",
-      "This is my realm after all...",
-      "Not everyone is as great as me.",
+      "hi",
+      "what took you so long",
+      "oh well",
+      "idc really",
     ],
   },
   realityEnter: {
     id: 2,
     lines: [
-      "Good luck with that!",
-      "You will need it.",
-      "My reality is flawless. You will fail.",
+      "ba",
     ],
   },
   realityComplete: {
     id: 3,
     lines: [
-      "So fast...",
-      "Do not think so much of yourself.",
-      "This is just the beginning.",
-      "You will never be better than me.",
+      "ae ligature",
     ],
   },
   achievement1: {
     id: 4,
     requirement: () => V.spaceTheorems >= 1,
     lines: [
-      "Only one? Pathetic.",
-      "Your accomplishments pale in comparison to mine.",
+      "e",
+      "that took you a while",
+      "i wonder why",
     ],
   },
   achievement6: {
     id: 5,
     requirement: () => V.spaceTheorems >= 6,
     lines: [
-      "This is nothing.",
-      "Do not be so full of yourself.",
+      "it's only one",
+      "idrc",
     ],
   },
   hex1: {
     id: 6,
     requirement: () => player.celestials.v.runUnlocks.filter(a => a === 6).length >= 1,
     lines: [
-      "Do not think it will get any easier from now on.",
-      "You are awfully proud for such a little achievement.",
+      "truly a gray moment",
+      "is there a reason to cheer",
     ],
   },
   achievement12: {
     id: 7,
-    requirement: () => V.spaceTheorems >= 12,
+    requirement: () => effarig.relicshards >= 1e30,
     lines: [
-      "How did you...",
-      "This barely amounts to anything!",
-      "You will never complete them all.",
+      "Ning thinking king in irving, wings in birding. Is liking birding. Is lying king. Is ling birding. I is liking birding and lying king. Is a misspelling of 'king in birding, I am interested in birding. I am not a king or lying. I like birding and am interested in it. It is not a lie.",
     ],
   },
   achievement24: {
     id: 8,
     requirement: () => V.spaceTheorems >= 24,
     lines: [
-      "Impossible...",
-      "After how difficult it was for me...",
+      "go get the 9th dimension instead",
     ],
   },
   hex3: {
     id: 9,
     requirement: () => player.celestials.v.runUnlocks.filter(a => a === 6).length >= 3,
     lines: [
-      "No... No... No...",
-      "This cannot be...",
+      "at least you can hop in on on",
     ],
   },
   allAchievements: {
     id: 10,
     requirement: () => V.spaceTheorems >= 36,
     lines: [
-      "I... how did you do it...",
-      "I worked so hard to get them...",
-      "I am the greatest...",
-      "No one is better than me...",
-      "No one... no one... no on-",
+      "rip",
     ],
   }
 };
